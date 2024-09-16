c08904010c2397f02ee38e1a6f48fbfc38d4a82e
Nocteln <eliottmieze@gmail.com>
2024-09-16 13:58:34 +0200

    continuation du about et debut du skills

---
+++ components/Skills.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="pt-10">
+    <div v-for="skill in skills" :key="skill.name">
+      <div>
+        <h1>{{ skill.name }}</h1>
+        <Icon :name="skill.icon" class="grayscale hover:grayscale-0 text-2xl" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import skills from "~/public/data/skills";
+//TODO: effet de paralax pour les icones
+</script>
---
+++ public/data/skills.js
@@ -0,0 +1,40 @@
+const skills = [
+  {
+    name: "HTML",
+    icon: "i-vscode-icons-file-type-html",
+  },
+  {
+    name: "CSS",
+    icon: "i-vscode-icons-file-type-css",
+  },
+  {
+    name: "JavaScript",
+    icon: "i-vscode-icons-file-type-js",
+  },
+  {
+    name: "React",
+    icon: "i-logos-react",
+  },
+  {
+    name: "NextJs",
+    icon: "i-devicon-nextjs",
+  },
+  {
+    name: "Vue",
+    icon: "i-logos-vue",
+  },
+  {
+    name: "nuxt",
+    icon: "i-logos-nuxt-icon",
+  },
+  {
+    name: "NodeJs",
+    icon: "i-logos-nodejs-icon",
+  },
+  {
+    name: "C#",
+    icon: "i-vscode-icons-file-type-csharp2",
+  },
+];
+
+export default skills;
